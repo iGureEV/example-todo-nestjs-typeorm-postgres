346e0a8e07ad162cf2e3035919d12d9de26ff6e6
Gureev Eugene <35802704+iGureEV@users.noreply.github.com>
2023-06-28 19:05:51 +0300

    Ошибка при попытке передать лишние атрибуты данных

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
